41c825ef195d70cef6c7d6bf83b2c66e8f5307b0
Manasi Sharma <110293421+manasi-sh@users.noreply.github.com>
2024-05-09 14:56:32 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -19,7 +19,7 @@ export const signup = async (req, res) => {
     });
     const token = jwt.sign(
       { email: newUser.email, id: newUser._id },
-      process.env.JWT_SECRET,
+      "test",
       { expiresIn: "1h" }
     );
     res.status(200).json({ result: newUser, token });
@@ -41,7 +41,7 @@ export const login = async (req, res) => {
     }
     const token = jwt.sign(
       { email: existinguser.email, id: existinguser._id },
-      process.env.JWT_SECRET,
+      "test",
       { expiresIn: "1h" }
     );
     res.status(200).json({ result: existinguser, token });
